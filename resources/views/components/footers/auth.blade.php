75b18a71c698245b0da935dffaa23e91ac34a1ff
ianjonlin <ianjonlin@gmail.com>
2022-11-02 21:08:54 +0800

    Auth Footer

--- resources/views/components/footers/auth.blade.php
+++ resources/views/components/footers/auth.blade.php
@@ -3,35 +3,35 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-start">
-                    © <script>
+                    Copyright ©
+                    <script>
                         document.write(new Date().getFullYear())
-
-                    </script>,
-                    made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a> &
-                    <a href="https://www.updivision.com" class="font-weight-bold" target="_blank"> UPDIVISION</a>
-                    for a better web.
+                    </script>
+                    <b>TAR UC. ALL RIGHTS RESERVED</b>
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative
-                            Tim</a>
+                        <a href="https://www.tarc.edu.my/disclaimer.htm" class="nav-link text-muted"
+                            target="_blank">Disclaimer</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.updivision.com" class="nav-link text-muted" target="_blank">UPDIVISION</a>
+                        <a href="https://www.tarc.edu.my/privacy.htm" class="nav-link text-muted"
+                            target="_blank">Privacy Policy</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted"
-                            target="_blank">About Us</a>
+                        <a href="https://twitter.com/tarctechnology" class="nav-link text-muted" target="_blank"><i
+                                class="fab fa-twitter" aria-hidden="true"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+                        <a href="https://www.facebook.com/tunkuabdulrahmanuniversitycollege" class="nav-link text-muted"
+                            target="_blank"><i class="fab fa-facebook" aria-hidden="true"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
-                            target="_blank">License</a>
+                        <a href="https://www.linkedin.com/school/tunku-abdul-rahman-university-college/"
+                            class="nav-link text-muted" target="_blank"><i class="fab fa-linkedin"
+                                aria-hidden="true"></i></a>
                     </li>
                 </ul>
             </div>
